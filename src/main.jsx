db491e3b1ed69ab10176bc5b829c8606afa4442f
farooq-teqniqly <farooq@teqniqly.com>
2025-10-01 14:09:27 -0700

    chore: Add react-query-devtools for enhanced debugging and integrate it into the app.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 
@@ -10,6 +12,10 @@ createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <StrictMode>
       <App />
+      <ReactQueryDevtools
+        initialIsOpen={false}
+        position="bottom"
+      ></ReactQueryDevtools>
     </StrictMode>
   </QueryClientProvider>
 );
