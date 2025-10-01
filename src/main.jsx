fe71bce93c8490e287aba1abd2ec9109440c4176
farooq-teqniqly <farooq@teqniqly.com>
2025-10-01 12:58:03 -0700

    chore: Add react-query and react-icons dependencies with usage setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </QueryClientProvider>
 );
